62eeb238736d2b523638ee63e7f3f87dc8da5f48
ishani <91791452+deathmukh@users.noreply.github.com>
2023-11-06 15:10:12 +0530

    icons directory changed

--- chit-talk-client/src/components/Groups.js
+++ chit-talk-client/src/components/Groups.js
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from '../icons/communication.png';
 import SearchIcon from '@mui/icons-material/Search';
 import { IconButton } from '@mui/material';
+
 export default function Groups() {
     return (
         <div className='list-container'>
