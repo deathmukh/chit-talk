65f02ee87b0ca460bbf2d89f5e8ef99fb0413d6b
ishani <91791452+deathmukh@users.noreply.github.com>
2023-11-06 15:10:58 +0530

    updates

--- chit-talk-server/index.js
+++ chit-talk-server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("api chaltoy")
+    res.send("api chaltoy hehehe")
 });
 
 app.listen(5000, console.log("Server Running"));
