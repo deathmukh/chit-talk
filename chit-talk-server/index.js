192949358f61ce6229ec260f5f48f74e01d9f8bf
ishani <91791452+deathmukh@users.noreply.github.com>
2023-11-08 14:43:09 +0530

    connected monngodb to server

--- chit-talk-server/index.js
+++ chit-talk-server/index.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const dotenv = require("dotenv");
+const { default: mongoose } = require('mongoose');
 
 const app = express();
+dotenv.config()
 
+const connectDb = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI)
+        console.log("Server is connected to database")
+    }
+    catch (err) {
+        console.log("Error connecting to database",err.message)
+    }
+
+}
+connectDb();
 app.get("/", (req, res) => {
     res.send("api chaltoy hehehe")
 });
 
-app.listen(5000, console.log("Server Running"));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log("Server Running"));
