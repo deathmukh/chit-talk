4d966ad8b654c30e7423279eb5d2485d8ab2108f
ishani <91791452+deathmukh@users.noreply.github.com>
2023-11-16 14:26:50 +0530

    features update

--- chit-talk-client/src/App.js
+++ chit-talk-client/src/App.js
@@ -1,36 +1,31 @@
-import React from 'react'
-import './App.css';
-import MainContainer from './components/mainContainer';
-import Login from './components/Login';
-import { Route, Routes } from 'react-router-dom';
-import Welcome from './components/Welcome';
-import ChatArea from './components/ChatArea';
-import CreateGroups from './components/CreateGroups';
-import Users from './components/Users';
-import Groups from './components/Groups';
+import React from "react";
+import "./App.css";
+import MainContainer from "../src/components/MainContainer";
+import Login from "../src/components/Login";
+import { Route, Routes } from "react-router-dom";
+import Welcome from "../src/components/Welcome";
+import ChatArea from "../src/components/ChatArea";
+import Users from "../src/components/Users";
+import CreateGroups from "../src/components/CreateGroups";
+import Groups from "../src/components/Groups";
+import { useDispatch, useSelector } from "react-redux";
+
 function App() {
+  const lightTheme = useSelector((state) => state.themeKey);
   return (
-    <div className='App'>
+    <div className={"App" + (lightTheme ? "" : "-dark")}>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="app" element={<MainContainer />} >
-          <Route path="welcome" element={<Welcome />}>
-          </Route>
-          <Route path="chat" element={<ChatArea />}>
-          </Route>
-          <Route path="users" element={<Users />}>
-          </Route>
-          <Route path="groups" element={<Groups />}>
-          </Route>
-          <Route path="create-groups" element={<CreateGroups />}>
-          </Route>
-
-
+        <Route path="app" element={<MainContainer />}>
+          <Route path="welcome" element={<Welcome />}></Route>
+          <Route path="chat/:_id" element={<ChatArea />}></Route>
+          <Route path="users" element={<Users />}></Route>
+          <Route path="groups" element={<Groups />}></Route>
+          <Route path="create-groups" element={<CreateGroups />}></Route>
         </Route>
-
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- chit-talk-client/src/components/MessageSelf.js
+++ chit-talk-client/src/components/MessageSelf.js
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from "react";
 
-function MessageSelf() {
-  var props2 = {name: "You", message: "this is a sample text"}
-
+function MessageSelf({ props }) {
   return (
-    <div className='self-message-container'>
-      <div className='messageBox'>
-        <p>{props2.message}</p>
-        <p className='self-timeStamp'>12:00am</p>
+    <div className="self-message-container">
+      <div className="messageBox">
+        <p style={{ color: "black" }}>{props.content}</p>
+       
       </div>
     </div>
-  )
+  );
 }
 
-export default MessageSelf
+export default MessageSelf;
